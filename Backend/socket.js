600b9aa0b5c5d4a48fc8cadea93629a3f8787ed0
Sarthak207 <sarthakpardeshi207@gmail.com>
2025-08-02 15:41:34 +0530

    Changed CORS frontend links

--- Backend/socket.js
+++ Backend/socket.js
@@ -7,7 +7,7 @@ let io;
 function initializeSocket(server) {
     io = socketIo(server, {
         cors: {
-            origin: 'https://hopinn-1.onrender.com',
+            origin: 'https://hop-inn.vercel.app',
             methods: [ 'GET', 'POST' ]
         }
     });
